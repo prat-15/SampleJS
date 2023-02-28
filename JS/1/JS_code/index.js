aeb77f142fa9281c2ee307b95b5fbc4a3fa1f6f1
Mahajan <Pratik.Mahajan@allscripts.com>
2023-02-28 13:58:39 +0530

    JS sample application

---
+++ JS/1/JS_code/index.js
@@ -0,0 +1,9 @@
+console.time('Your code took')
+console.log("Hello Console")
+console.table({Harry : "80", 
+Pratik : "90"})
+console.warn("This is warning")
+console.timeEnd("Your code took")
+//console.clear()
+// console.assert(500<189, 'This is not possible.')
+console.error('This is an error')
